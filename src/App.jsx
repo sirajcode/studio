5720d89916d8ce12da2b0673d7a9eac0412b3113
siraj <sirajbhutta50@gmail.com>
2022-10-11 19:51:31 +0500

    First commit: file and folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './compnents/Navbar';
+import Home from './compnents/Home';
+const App = () => {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/compnents/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <>
+        <div>
+            this is home component
+        </div>
+        </>
+    )
+}
+
+export default Home
