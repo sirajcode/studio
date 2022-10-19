47f3b2f410909bf733bc62a5321763d2e0151c92
siraj <sirajbhutta50@gmail.com>
2022-10-19 22:20:38 +0500

    feat(App): Studio UI Navigation
    - starting with UI of Studio App
    - Refinig the DesignSystem Alonge With

--- src/App/Component/Navbar.jsx
+++ src/App/Component/Navbar.jsx
@@ -3,13 +3,40 @@ import Button from '../../DesignSystem/Button';
 import Container from '../../DesignSystem/Container';
 import Section from '../../DesignSystem/Section';
 import Flex from '../../DesignSystem/Utils';
+import Text from '../../DesignSystem/Text'
+
+// TODO:
+//1. creating multiple navigations
+//  1.1 There are three navigation in design system ui
+//      1.1.1 top navigation /
+//      1.1.2 side navigation
+//      1.1.3 bottom navigation
+// HINTS:
+//1) All Navigations will fixed at thier postion
+
+
+
+
 const Navbar = () => {
     return (
-        <div>
-            <Container width='sm' >
-              this is sm container    
-         </Container>          
-        </div>
+
+        <Section bg='green' padding={3}>
+            <Flex justify='SpaceBetween'>
+                <Flex>
+                        <Text fontSize={2} marginLeft={2} weight='600'>Logo</Text>
+                        <Text fontSize={2} marginLeft={4} weight='600'>File</Text>
+                        <Text fontSize={2} marginLeft={4} weight='600'>Text </Text>
+                        <Text fontSize={2} marginLeft={4} weight='600'>Color</Text>                   
+                        <Text fontSize={2} marginLeft={4} weight='600'>View</Text>                   
+                </Flex>
+                <Section  fontSize={2} weight='600'>
+                this is navigation section
+                </Section> 
+                <Section  fontSize={2} marginRight={4} weight='600'>
+                this is navigation section
+                </Section> 
+            </Flex>
+        </Section>
     )
 }
 
