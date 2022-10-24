c05815f5a15f16cdabe587a625faaf4e93d5f98c
siraj <sirajbhutta50@gmail.com>
2022-10-24 12:34:39 +0500

    refacter(App): Header icons

--- src/App/Pages/Header.jsx
+++ src/App/Pages/Header.jsx
@@ -1,22 +1,30 @@
 import React from 'react';
-import Section from '../../DesignSystem/Section';
 import { BsHeartFill } from "react-icons/bs";
+import Section from '../../DesignSystem/Section';
 import Text from "../../DesignSystem/Text";
 import Flex from "../../DesignSystem/Utils";
+import Container from "../../DesignSystem/Container";
+import { VscSettingsGear  } from "react-icons/vsc";
 
 const Header = () => {
-    return (
-        <>
-            <Section bg='green'>
-                <Flex FLexDirection='Column' alignItems='Center' Justify='Center' style={{ height: '100vh'}}>
-                    <Text><BsHeartFill size={200} /></Text>
-                    <Text weight='600'>npm: npm install studiods</Text>         
-                    <Text weight='600'>this is text</Text>         
-                    <Text weight='600'>this is text</Text>         
-                    <Text weight='600'>this is text</Text>         
-               </Flex>
-            </Section>
-        </>
+  return (
+    <>
+      <Container width='sm'>
+        <Section marginTop={6} marginBottom={6}>
+          <Flex>
+              <Text marginRight={6} >
+              <BsHeartFill size={120} />
+              </Text>
+              <Section>
+                  <Text fontSize={3} weight='600'> Studio | Dashboard and Ui Design System</Text>
+                  <Text weight='600'>@engsiraj</Text>
+                  <Text fontSize={2}>Ui Design system builds with reactjs</Text>
+              <Text weight='600' fontSize={2} > V.0.1.1 <VscSettingsGear size={14} /></Text>
+            </Section>   
+          </Flex>
+        </Section>  
+      </Container>
+      </>
   )
 }
 
