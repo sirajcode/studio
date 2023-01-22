52c189a08612e65bce413b730d85ee2ece0c4114
siraj <sirajbhutta50@gmail.com>
2023-01-22 21:20:28 +0500

    🌎 style(DesignSystem): addition to variables

--- src/DesignSystem/Variable.jsx
+++ src/DesignSystem/Variable.jsx
@@ -1,4 +1,3 @@
-
 // todo
 // - defining varibales for design system
 
@@ -30,9 +29,35 @@ fontWieght.normal = fontWieght[0];
 fontWieght.strong = fontWieght[1];
 fontWieght.bold = fontWieght[2];
 
+//button sizes
+export const BtnSize = ["4px 9px", "7px 18px", "12px 20px"];
+BtnSize.small = BtnSize[0];
+BtnSize.medium = BtnSize[1];
+BtnSize.large = BtnSize[2];
+
+ //Border widths
+export const borderWidth = ["2px", "3px", "4px"];
+borderWidth.small = borderWidth[0];
+borderWidth.medium = borderWidth[1];
+borderWidth.large = borderWidth[2];
+
+// corners
+export const RoundConers = ["2px", "3px", "4px", "9999px"];
+RoundConers.small = RoundConers[0];
+RoundConers.medium = RoundConers[1];
+RoundConers.large = RoundConers[2];
+RoundConers.exlarge = RoundConers[3];
+
+//spaces
+export const spaces = ["4px", "8px", "16px", "32px", "48px", "56px"];
+spaces.smaller = spaces[0];
+spaces.small = spaces[1];
+spaces.medium = spaces[2];
+spaces.large = spaces[3];
+spaces.larger = spaces[4];
+spaces.extraLarge = spaces[5];
 
 // not defined in design system yet
-
 
 //break points
 const breakpoints = ["600px", "900px", "1200px", "1800px"];
@@ -41,13 +66,4 @@ breakpoints.md = breakpoints[1];
 breakpoints.lg = breakpoints[2];
 breakpoints.xl = breakpoints[3];
 
-//spaces
-const spaces = ["4px", "8px", "16px", "32px", "48px", "56px"];
-spaces.smaller = spaces[0];
-spaces.small = spaces[1];
-spaces.medium = spaces[2];
-spaces.large = spaces[3];
-spaces.larger = spaces[4];
-spaces.extraLarge = spaces[5];
 
- 
