1f4d144f584cca83da1014bcc1dd69f0aac3c0c2
siraj <sirajbhutta50@gmail.com>
2023-01-22 17:16:57 +0500

    🌎 style(DesignSystem): scope variables

---
+++ src/DesignSystem/Variable.jsx
@@ -0,0 +1,53 @@
+
+// todo
+// - defining varibales for design system
+
+//colors
+export const colors= {
+        primary: "#ED4B9E",
+        secondary: "#4B4DED",
+        tertiary: "#F3D9DA",
+        dark: "#0E0E2C",
+        success: "#31D0AA",
+        text: "#4A4A68",
+        subtleText: "#8C8CA1",
+        accent: "#ECF1F4",
+        light: "#FAFCFE",
+        white: "#FFFFFF",
+}
+
+//font - sizes   
+export const fontSize = ["14px", "18px", "24px", "30px", "36px"];
+fontSize.text = fontSize[0];
+fontSize.regular = fontSize[1];
+fontSize.small = fontSize[2];
+fontSize.medium = fontSize[3];
+fontSize.large = fontSize[4];
+
+//font - wieght
+export const fontWieght = ["400","500","600"];
+fontWieght.normal = fontWieght[0];
+fontWieght.strong = fontWieght[1];
+fontWieght.bold = fontWieght[2];
+
+
+// not defined in design system yet
+
+
+//break points
+const breakpoints = ["600px", "900px", "1200px", "1800px"];
+breakpoints.sm = breakpoints[0];
+breakpoints.md = breakpoints[1];
+breakpoints.lg = breakpoints[2];
+breakpoints.xl = breakpoints[3];
+
+//spaces
+const spaces = ["4px", "8px", "16px", "32px", "48px", "56px"];
+spaces.smaller = spaces[0];
+spaces.small = spaces[1];
+spaces.medium = spaces[2];
+spaces.large = spaces[3];
+spaces.larger = spaces[4];
+spaces.extraLarge = spaces[5];
+
+ 
