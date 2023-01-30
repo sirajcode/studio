9042ee44a111a2385269094d2d4b47723d6de01f
siraj <sirajbhutta50@gmail.com>
2023-01-30 10:53:55 +0500

    🧮 style(library): container setup

--- src/Component/library/common/Container.jsx
+++ src/Component/library/common/Container.jsx
@@ -1,19 +1,33 @@
-import styled from 'styled-components'
+import styled,{css} from 'styled-components'
 
 const Container = styled.div`
-margin-left:13%;
-margin-right:13%;
-//small container : sm
-padding-right:${props => props.sm ? 'calc((70vw - 768px) / 2)' : 'none'};
-padding-left: ${props => props.sm ? 'calc((70vw - 768px) / 2)' : 'none'};
-
-//medium container : med
-padding-right:${props => props.med ? 'calc((80vw - 960px) / 2)' : 'none'};
-padding-left: ${props => props.med ? 'calc((80vw - 960px) / 2)' : 'none'};
-
-//large container : lg
-padding-right:${props => props.lg ? 'calc((90vw - 1200px) / 2)' : 'none'};
-padding-left: ${props => props.lg ? 'calc((90vw - 1200px) / 2)' : 'none'};
+margin: 0px auto;
+${p => {
+    if (p.dashboard) {
+        return css`
+            padding-left:calc(55px + 3%);
+            padding-right: 3%;
+        `
+    }
+     else if (p.sm) {
+        return css`
+            padding-left:20%;
+            padding-right: 20%;
+        `
+    }
+     else if (p.med) {
+        return css`
+            padding-left:15%;
+            padding-right: 15%;
+        `
+    }
+     else if (p.lg) {
+        return css`
+            padding-left:10%;
+            padding-right: 10%;
+        `
+    }
+}}
 `
 export default Container
 
