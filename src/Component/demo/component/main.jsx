f4b0b6177e36aae3d5f3592024d13a263daaefcc
siraj <sirajbhutta50@gmail.com>
2023-02-05 11:55:22 +0500

    🎊 Docs(webapp): component page

--- src/Component/demo/component/main.jsx
+++ src/Component/demo/component/main.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import styled from 'styled-components'
 import { Section, Text, Flex, Column } from '../App'
 
+
+
+
+/*
+todo: Components page 
+------------
+? sections
+! - container width  width='8'
+
+
+
+------------
+*/
+
+
 const Wrapper = styled.div`
   padding-top: 5%;
   padding-bottom: 5%;
@@ -14,58 +29,57 @@ const Wrapper = styled.div`
 const Components = () => {
   return (
     <Wrapper>
-      <Section Top='6'>
-        <Text darktext smallHeading strong Bottom='4' >Components</Text>
+      <Section  width='8'>
+      <Text darktext largeHeading bold Bottom='4' >Components</Text>
+      <Text Bottom='6' darklabel> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, accusamus.</Text>
         <Flex justifyContent='space-between' >
-
-          <Column five border round Padded='4' bg='bgc'>
+          <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
           </Column>
-
-           <Column five border round Padded='4' bg='bgc'>
+            <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
           </Column>
-           <Column five border round Padded='4' bg='bgc'>
+            <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
           </Column>
-           <Column five border round Padded='4' bg='bgc'>
+            <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
           </Column>
-           <Column five border round Padded='4' bg='bgc'>
-          <Text regular strong darktext Bottom='1'>Grid</Text>
-          <Text darkdisable>grid system props</Text>
-          </Column>
-           
         </Flex>
-        <Section Top='4'>
+        <Section Top='3'>
         <Flex justifyContent='space-between' >
-          <Column five border round Padded='4' bg='bgc'>
+           <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
-          </Column>
-           <Column five border round Padded='4' bg='bgc'>
+          </Column>  <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
-          </Column>
-           <Column five border round Padded='4' bg='bgc'>
+          </Column>  <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
-          </Column>
-           <Column five border round Padded='4' bg='bgc'>
-          <Text regular strong darktext Bottom='1'>Grid</Text>
-          <Text darkdisable>grid system props</Text>
-          </Column>
-           <Column five border round Padded='4' bg='bgc'>
+          </Column>  <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
           </Column>
           </Flex>
-          </Section>
         </Section>
+      </Section>
+        <Text darktext smallHeading Top='6' Bottom='4'> Component name </Text>
+        <Text darklabel  Bottom='4'> props and description show etc</Text>
+          <Section darklabel>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          </Section>
+
+
     </Wrapper>
   )
 }
