4311e39e10d7650ed5f5172702d5c55e091e9a42
siraj <sirajbhutta50@gmail.com>
2023-02-05 11:57:40 +0500

    📄 docs(webapp): layout page

--- src/Component/demo/layout/main.jsx
+++ src/Component/demo/layout/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 import { Section, Text, Flex, Column, Link} from '../App'
 
+/*
+todo: layout components 
+
+*/
+
 const Wrapper = styled.div`
   padding-top: 5%;
   padding-bottom: 5%;
@@ -11,28 +16,56 @@ const Wrapper = styled.div`
 `
 const Layout = () => {
   return (
-    <Wrapper>
-       <Section Top='6'>
-        <Text darktext smallHeading strong Bottom='4' >Layout</Text>
+     <Wrapper>
+      <Section  width='8'>
+      <Text darktext largeHeading bold Bottom='4' >layout</Text>
+      <Text Bottom='6' darklabel> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, accusamus.</Text>
         <Flex justifyContent='space-between' >
-          <Column four border round Padded='4' bg='bgc'>
+          <Column four border round Padded='3' bg='bgc'>
           <Text regular strong darktext Bottom='1'>Grid</Text>
           <Text darkdisable>grid system props</Text>
           </Column>
-            <Column four border round Padded='4' bg='bgc'>
-          <Text regular strong darktext Bottom='1'>Container</Text>
-          <Text darkdisable>layout first component</Text>
+            <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
           </Column>
-            <Column four border round Padded='4' bg='bgc'>
-          <Text regular strong darktext Bottom='1'>Width</Text>
-          <Text darkdisable>layout main part</Text>
+            <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
           </Column>
-            <Column four border round Padded='4' bg='bgc'>
-         <Text regular strong Bottom='1'><Link darktext>Section</Link></Text>
-          <Text darkdisable>div element enhance with props</Text>
+            <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
           </Column>
         </Flex>
+        <Section Top='3'>
+        <Flex justifyContent='space-between' >
+           <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
+          </Column>  <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
+          </Column>  <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
+          </Column>  <Column four border round Padded='3' bg='bgc'>
+          <Text regular strong darktext Bottom='1'>Grid</Text>
+          <Text darkdisable>grid system props</Text>
+          </Column>
+          </Flex>
         </Section>
+      </Section>
+        <Text darktext smallHeading Top='6' Bottom='4'> layput name </Text>
+        <Text darklabel  Bottom='4'> props and description show etc</Text>
+          <Section darklabel>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          <li>this is some text here </li>
+          </Section>
     </Wrapper>
   )
 }
