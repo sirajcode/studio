6486d7d134a03e98930ee2ccc001930024b215f9
siraj <sirajbhutta50@gmail.com>
2023-02-05 12:10:06 +0500

    🌐 style(library): addition in props

--- src/Component/demo/App.jsx
+++ src/Component/demo/App.jsx
@@ -1,10 +1,21 @@
-//library imports 
+/*
+todo: library imports 
+- library all files/images/components/pages etc.....
+*/
 import {Button,Link} from "../library/Button";
 import Alert from "../library/Alert";
 import Container from "../library/Container";
 import Text from "../library/Text";
 import GlobalStyles from '../library/GlobalStyles';
 import { Section, Flex, Column } from '../library/Util';
-import img from '../assets/headerimage.png'
 
-export {GlobalStyles, Button, Link, Alert, Container, Text, Section, Flex, Column, img}
+export { GlobalStyles, Button, Link, Alert, Container, Text, Section, Flex, Column }
+// header
+import headerimg from '../assets/header.png';
+// landing page image
+import design from '../assets/ds1.png';
+import library from '../assets/ds2.png';
+import component from '../assets/ds3.png';
+
+export { headerimg, design, library, component}
+
