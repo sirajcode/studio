11aa3efe7bccaa5e9e0530aa787d3481f6d22d70
siraj <sirajbhutta50@gmail.com>
2023-02-25 16:08:50 +0500

    update(styles): design system

--- dist/index.jsx
+++ dist/index.jsx
@@ -1,3 +1,8 @@
-import { Button, StyledLink } from './Button';
-import Text from './Text';
+import { border } from './lib/border';
+import { color } from './lib/color';
+import { flexbox, grid, layout, background, shadow } from './lib/layout';
+import { space } from './lib/space';
+import { typography } from './lib/typo';
 
+
+export { flexbox, layout, grid, background, shadow, typography, space, color, border }
