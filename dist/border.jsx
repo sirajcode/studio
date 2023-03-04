d2b138ac15c13ae526c0cc8a450deb4a83e2f5d0
siraj <sirajbhutta50@gmail.com>
2023-03-04 18:38:11 +0500

    🔳 style: border

--- dist/border.jsx
+++ dist/border.jsx
@@ -14,11 +14,15 @@ for (const pro in vars) {
 
 const border = css`
 border:${p => p.border};
+border-top: ${p => p.borderTop};
+border-left: ${p => p.borderLeft};
+border-right: ${p => p.borderRight};
+border-bottom: ${p => p.borderBottom};
 border-width: ${p => p.borderWidth};
 border-style: ${p => p.borderStyle};
 border-color:${p => p.borderColor};
 border-color: ${BorderColor};
-border-radius: ${p => p.corner};
+border-radius: ${p => p.corner}; 
 
 `
 
